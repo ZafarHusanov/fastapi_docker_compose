a414a21bb152a44c9e0eb3ba5806367882a73c7a
z <you@example.com>
2024-09-06 15:18:38 +0500

    ORM change BIGINT

--- src/models/users/model_users.py
+++ src/models/users/model_users.py
@@ -1,6 +1,6 @@
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Column, Integer
+from sqlalchemy import String, Column, Integer, BIGINT
 
 
 class Base(DeclarativeBase):
@@ -9,20 +9,20 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-            self.name,
-            self.fullname,
-            self.nickname,
-        )
+    # id = Column(Integer, primary_key=True)
+    # name = Column(String)
+    # fullname = Column(String)
+    # nickname = Column(String)
+    # def __repr__(self):
+    #     return "<User(name='%s', fullname='%s', nickname='%s')>" % (
+    #         self.name,
+    #         self.fullname,
+    #         self.nickname,
+    #     )
 
-    # id: Mapped[int] = mapped_column(primary_key=True)
-    # username: Mapped[str] = mapped_column(String(50))
-    # password: Mapped[str] = mapped_column(String())
-    #
-    # def __repr__(self) -> str:
-    #     return f"User(id={self.id!r}, username={self.username!r})"
+    id: Mapped[int] = mapped_column(BIGINT, primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    password: Mapped[str] = mapped_column(String())
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
