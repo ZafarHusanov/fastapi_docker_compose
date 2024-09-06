9efcbca79813c5b2a0196ac29bf49f828ea7168d
z <you@example.com>
2024-09-06 14:40:18 +0500

    ORM change

--- src/models/users/model_users.py
+++ src/models/users/model_users.py
@@ -1,6 +1,6 @@
-from sqlalchemy import String
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Column, Integer
 
 
 class Base(DeclarativeBase):
@@ -9,9 +9,20 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(50))
-    password: Mapped[str] = mapped_column(String())
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
+            self.name,
+            self.fullname,
+            self.nickname,
+        )
 
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, username={self.username!r})"
+    # id: Mapped[int] = mapped_column(primary_key=True)
+    # username: Mapped[str] = mapped_column(String(50))
+    # password: Mapped[str] = mapped_column(String())
+    #
+    # def __repr__(self) -> str:
+    #     return f"User(id={self.id!r}, username={self.username!r})"
